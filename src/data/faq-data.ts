7283494cdf51dba2b4d0d2f61c4fbeca68b2dde8
jaz <zayas.j10@gmail.com>
2025-06-02 00:12:26 +0800

    feat: update FAQ answers and add parking information; adjust route names in map section

--- src/data/faq-data.ts
+++ src/data/faq-data.ts
@@ -7,17 +7,17 @@ export const faqData: FaqItem[] = [
   {
     question: 'How do I book the Moraine Lake & Lake Louise Shuttle?',
     answer:
-      'You can Choose Route 1 or Route 2.<br/><br/>Route 1 goes to Lake Louise 1st and Moraine Lake after.<br/><br/>Route 2 goes to Moraine Lake 1st and Lake Louise after.<br/><br/>You have 14 times slot to choose in each route. You can get your itinerary the way you want it.',
+      'You can click the Route Map, Time table of book now button<br/><br/>You have 6 times slot to choose to suit your Itineraries.',
   },
   {
     question: 'Can I bring my pet?',
     answer:
-      '1 Pet is allowed per passenger.<br/><br/>Pet price is as followed:<br/><br/>24lbs or less $35 lap seated with the owner.<br/><br/>25lbs or more $44.98 required a seat.<br/><br/>Disrupted pets will be turned away without refund.',
+      '1 Pet is allowed per passenger.<br/><br/>Pet price is as followed:<br/><br/>24lbs or less $30 lap seated with the owner.<br/><br/>25lbs or more $75 required a seat.<br/><br/>Disrupted pets will be turned away without refund.',
   },
   {
     question: 'Does Children Required Car seats?',
     answer:
-      "No but recommended.<br/><br/>According to Alberta's Occupant Restraint Laws, specific exemptions exist for vehicles such as taxis, limousines, shuttle buses, and party buses. These vehicles are classified differently from personal cars and are not required to have car seats or even seat belts for every passenger. For example, the same law applies to school buses, where children can travel without seat belts. Occupant Restraint Laws",
+      "No but recommended.<br/><br/>According to Alberta's Occupant Restraint Laws, specific exemptions exist for vehicles such as taxis, limousines, shuttle buses, and party buses. These vehicles are classified differently from personal cars and are not required to have car seats or even seat belts for every passenger. For example, the same law applies to school buses, where children can travel without seat belts. <a class='underline' href='https://www.alberta.ca/occupant-restraint-laws' target='_blank'>Occupant Restraint Laws</a>",
   },
   {
     question: 'Is infant free?',
@@ -27,7 +27,12 @@ export const faqData: FaqItem[] = [
   {
     question: 'Is there a Group discount?',
     answer:
-      'Yes. Group discount as follows:<br/><br/>10-14 = 5%<br/><br/>15+ = 10%<br/><br/>Will automatically reflect on your total bill at check out and only done by booking directly to the website or calling the admin.',
+      'Yes. Group discount as follows:<br/><br/>10-14 = 5%<br/><br/>15+ = 10%<br/><br/>There is an option for you to click on group discount.',
+  },
+  {
+    question: 'Where to park and where to meet?',
+    answer:
+      'Banff Train Station: 327 Railway Ave, Banff, AB T1L 1A1<br/><br/>Wait at the front of the train station entrance<br/><br/>Shuttle will arrive 5min before departure time.',
   },
   {
     question: 'What if I missed my shuttle?',
@@ -37,7 +42,7 @@ export const faqData: FaqItem[] = [
   {
     question: 'What if my Shuttle is late?',
     answer:
-      "Most of the time shuttle arrived 10min prior to departure time.<br/><br/>In some instance we can't control the road traffic. Just be in the said pick location and the driver will announce the time slot what he/she is picking up and scan your ticket.",
+      "Most of the time shuttle arrived 5min-10min prior to departure time.<br/><br/>Staff will advise you if the shuttle will be late.<br/><br/>In some instance we can't control the road traffic. Just be in the said pick location and the driver will announce the time slot what he/she is picking up and scan your ticket.",
   },
   {
     question: 'How much time I can spend in the lakes?',
@@ -61,7 +66,7 @@ export const faqData: FaqItem[] = [
   {
     question: 'What if I/we changed our plan can we get a refund?',
     answer:
-      'You can do a self cancellation on your reservation with in 72 HOURS prior on your departure and get a full REFUND.<br/><br/>After 72 Hours you can only change your schedule time slot on any available dates up to 48 HOURS prior on your schedule time.<br/><br/>Anything above mention there will be no refund. Ticket good as Sold.',
+      'You can do a self cancellation on your reservation with in 72 HOURS prior on your departure and get a full REFUND.<br/><br/>After 72 Hours you can only change your schedule time slot on any available dates up to 48HOURS prior on your schedule time.<br/><br/>Anything above mention there will be no refund. Ticket good as Sold.',
   },
   {
     question:
@@ -69,9 +74,4 @@ export const faqData: FaqItem[] = [
     answer:
       'You will be put on the waiting list on your desired schedule and will be prompted by email on or before 48 HOURS prior to your desired schedule.<br/><br/>Or you can go to the terminal for a chance passenger 15 min prior on your desired schedule but no guaranteed.',
   },
-  // {
-  //   question: 'WHERE CAN I PARK MY VEHICLE? ',
-  //   answer:
-  //     'Free park at the overflow parking at Lake Louise summer gondola and their are free ride for parks canada going to samson mall. Just check their time slot.',
-  // },
 ];
